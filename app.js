a7b9d212a3105d83f5a9920f4de9f5033f8a5e69
unknown <sadaddinovagulsanam393@gmail.com>
2025-10-25 16:32:14 +0300

    push

---
+++ app.js
@@ -0,0 +1,12 @@
+const item = document.getElementsByClassName("item")
+const idElement =document.getElementById("id-element")
+const spanEl = document.getElementsByTagName("span")
+const text = document.getElementById("text")
+
+
+item[0].innerHTML ="<h1 id=text>Hello</h1>"
+
+console.log(item[0])
+console.log(idElement.textContent)
+console.log(spanEl[textContent]
+)
